5fbb58dac87c5d11ec6774002621b9c9561b7478
Mufeeda Ck <mufeedamhdali@gmail.com>
2025-04-26 17:08:28 +0530

    #7

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -276,8 +276,8 @@ function ProductList() {
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/paradise-nursery-shopping-cart-app/" style={{ textDecoration: 'none' }}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                                <h3 style={{ color: 'white' }}>Plant Nursery</h3>
+                                <i style={{ color: 'white' }}>Come and take yours</i>
                             </div>
                         </a>
                     </div>
